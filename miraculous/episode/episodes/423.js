74eebf8b8c2e0f84b6dc348d5e5809081cc2db64
Potato <z4bakedpotato@gmail.com>
2022-01-27 01:39:25 -0800

    kuro neko rating

--- miraculous/episode/episodes/423.js
+++ miraculous/episode/episodes/423.js
@@ -1,4 +1,7 @@
+export const rating = 1;
 export const plot = true;
+export const lore = true;
 export const newMiraculous = true;
-export const comments = 'Not really a new holder but Adrien does get a costume change and a new name so? I\'m currently' +
-    'waiting for subs to be out to properly rate this episode though.';
+export const comments = 'Not really a new holder but Adrien does get a costume change and a new name so? I\'m not too ' +
+    'sure about the costume change being based on personality because if Mari "wasn\'t ready" at the beginning but she ' +
+    'is now, why isn\'t her costume changed all the time?';
